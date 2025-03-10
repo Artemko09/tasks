c2fecc45a97ec4f8d7110600750b2356efc8b226
Artemko09 <artem2005zhuk@gmail.com>
2025-03-10 02:27:11 +0200

    task 1 complete

---
+++ main.js
@@ -0,0 +1,33 @@
+let word = 'hello'
+let word1 = 'owu'
+let word2 = 'com'
+let country = 'ua'
+let num = 1
+let num2 = 10
+let num3 = -999
+let num4 = 123
+const PI = 3.14
+let num5 = 2.7
+let num6 = 16
+let yes = true
+let no = false
+// ----------------------------------------------------------
+let firstName = 'Zhuk'
+let midlleName = 'Artem'
+let lastName = 'Denisovuch'
+let person = firstName + ' ' + midlleName + ' ' + lastName
+let person1 = `${firstName} ${midlleName} ${lastName}`
+let person2 = [firstName, midlleName, lastName].join(' ')
+console.log(person)
+console.log(person1)
+console.log(person2)
+// ---------------------------------------------------------
+let a = 100
+let b = '100'
+let c = true
+console.log(typeof(a), typeof(b), typeof(c));
+// ---------------------------------------------------------
+first = prompt('Enter your first name')
+middle = prompt('Enter your middle name')
+last = prompt('Enter your last name')
+console.log(`${first} ${middle} ${last}`);
