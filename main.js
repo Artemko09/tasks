0a541e2b7f89c331536f0bafe1d687634a9c7f96
Artemko09 <artem2005zhuk@gmail.com>
2025-03-13 01:23:24 +0200

    Only task with cards left

--- main.js
+++ main.js
@@ -1,146 +1,63 @@
-function area(a, b){
-    return a * b
+console.log('hello world'.length);
+console.log('lorem ipsum'.length);
+console.log('javascript is cool'.length);
+// --------------------------------------
+console.log('hello world'.toUpperCase());
+console.log('lorem ipsum'.toUpperCase());
+console.log('javascript is cool'.toUpperCase());
+// --------------------------------------
+console.log('HELLO WORLD'.toLowerCase());
+console.log('LOREM IPSUM'.toLowerCase());
+console.log('JAVASCRIPT IS COOL'.toLowerCase());
+// --------------------------------------
+let str = '           dirty string   '
+console.log(str);
+console.log(str.trim());
+// --------------------------------------
+let str_2 = 'Ревуть воли як ясла повні'
+
+const stringToArray = (str) => {
+    return console.log(str.split(' '))
 }
 
-console.log(area(5, 2))
-// ------------------------------------
-function areaCircle(r){
-    return Math.PI * r ** 2
-}
-
-console.log(areaCircle(5));
-// -----------------------------------
-function areaCylinder(r, h){
-    return 2 * Math.PI * r * (h + r)
-}
-
-console.log(areaCylinder(5, 7))
-// -----------------------------------
-function takeArray(arr){
-    for(let el of arr){
-        console.log(el);
+stringToArray(str_2)
+// --------------------------------------
+const nums = [10, 8, -7, 55, 987, -1011, 0, 1050, 0]
+const strNums = nums.map(num => num.toString())
+console.log(nums);
+console.log(strNums);
+// --------------------------------------
+let num1 = [11,21,3];
+const sortNums = (arr, choice) => {
+    if(choice == '+'){
+        console.log(arr.sort((a, b) => a - b))
+    }
+    else if(choice == '-'){
+        console.log(arr.sort((a, b) => b - a))
     }
 }
 
-array = [1, 2, 3 ,4, 5, 6]
-console.log(takeArray(array));
-// ----------------------------------
-function typeSmth(text){
-    return document.write(`<p>${text}</p>`)
-}
-
-typeSmth('Hello, world!')
-// ----------------------------------
-function createList(text, howMuch){
-    document.write(`<ul>`)
-    for (let i = 0; i < howMuch; i++) {
-        document.write(`<li>${text}</li>`)
-    }
-    document.write(`</ul>`)
-}
-
-createList('Lol', 3)
-// ----------------------------------
-let arr = [1, 'Hello', true, 25, 'Walter']
-
-function createFromArray(array){
-    document.write(`<ul>`)
-    for (const el of array) {
-        document.write(`<li>${el}</li>`)
-    }
-    document.write(`</ul>`)
-}
-
-createFromArray(arr)
-// ---------------------------------
-let users = [
-    { id: 1, name: "Alice", age: 25 },
-    { id: 2, name: "Bob", age: 30 },
-    { id: 3, name: "Charlie", age: 22 },
-    { id: 4, name: "David", age: 28 },
-    { id: 5, name: "Eve", age: 35 }
+sortNums(num1, '+')
+sortNums(num1, '-')
+// -------------------------------------
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
 ];
 
-function divObject(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        document.write(`<div>`);
-        document.write(`<p>${arr[i].id}, ${arr[i].name}, ${arr[i].age}</p>`);
-        document.write(`</div>`);
-    }
-}
+coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration)
+let longCourses = coursesAndDurationArray.filter(cours => cours.monthDuration >= 5)
+const addIdCourses = coursesAndDurationArray.map((course, index) => ({
+    id: index += 1,
+    title: course.title, 
+    monthDuration: course.monthDuration
+}))
 
-divObject(users);
-// ---------------------------------
 
-const myArr = [34, 21, 10, 75, 38, 19]
-
-function minEl(array){
-    let min = array[0]
-    for (let i = 1; i < array.length; i++) {
-        if (min > array[i]){
-            min = array[i]
-        }
-    }
-    return min
-}
-
-console.log(minEl(myArr))
-// ---------------------------------
-const ar = [2, 5, 9, 4, 25]
-
-function sumOfArr(array){
-    let sum = array[0]
-    for (let i = 1; i < array.length; i++) {
-        sum += array[i]
-    }
-    return sum
-}
-
-console.log(sumOfArr(ar))
-// ----------------------------------
-function swap(arr, index1, index2) {
-    let temp = arr[index1];
-    
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-    
-    return arr;
-}
-
-const number = [11, 22, 33, 44];
-console.log(swap(number, 0, 1));
-// // -----------------------------------
-// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
-// Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
-
-const valutaVubir = [{currency: 'USD', value: 41.25}, {currency: 'EUR', value: 44.9}, {currency: 'PLN', value: 10.25}]
-
-function exchange(hryvni, arr_vubir){
-    let valuta = prompt('Enter the curency: ')
-        if(valuta == 'USD'){
-            console.log(`You got ${hryvni / arr_vubir[0].value} USD`);
-        }
-        else if (valuta == 'EUR'){
-            console.log(`You got ${hryvni / arr_vubir[1].value} EUR`);
-        }
-        else if (valuta == 'PLN'){
-            console.log(`You got ${hryvni / arr_vubir[2].value} PLN`);
-        }
-    
-}
-
-exchange(1000, valutaVubir)
-
-function exchange(hryvni, array, currency){
-    if(currency == array[0].currency){
-        console.log(`You got ${hryvni / array[0].value} USD`)
-    }
-    else if (currency == array[1].currency){
-        console.log(`You got ${hryvni / array[1].value} EUR`)
-    }
-    else if(currency == array[2].currency){
-        console.log(`You got ${hryvni / array[2].value} PLN`)
-    }
-}
-
-exchange(10000, valutaVubir,'PLN')
+console.log(coursesAndDurationArray);
+console.log(longCourses);
+console.log(addIdCourses);
